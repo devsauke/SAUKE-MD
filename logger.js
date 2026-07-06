07a2bce904a9a9dde75ddfec7ac39b828b1474e5
SAUKE <dkibiwott678@gmail.com>
2026-07-06 23:43:53 +0300

    Add logging functions with color-coded output

--- logger.js
+++ logger.js
@@ -1,1 +1,12 @@
 
+'use strict';
+const chalk = require('chalk');
+
+const tag = () => chalk.gray(`[${new Date().toLocaleTimeString()}]`);
+
+module.exports = {
+    info: (...args) => console.log(tag(), chalk.cyanBright('[SAYKE ND]'), ...args),
+    warn: (...args) => console.log(tag(), chalk.yellowBright('[SAYKE ND]'), ...args),
+    error: (...args) => console.log(tag(), chalk.redBright('[SAYKE ND]'), ...args),
+    success: (...args) => console.log(tag(), chalk.greenBright('[SAYKE ND]'), ...args),
+};
